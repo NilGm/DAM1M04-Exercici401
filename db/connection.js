3a5f9409ae3284c2bbd0e2aa9cf3ef0b585419a1
NilGm <nilgarciam19@gmail.com>
2026-05-19 21:12:50 +0200

    local,fin

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',          // Canvia-ho si el teu usuari no és root
-    password: 'P@ssw0rd',          // Posa aquí la teva contrasenya de MySQL
+    password: 'Calamot2023',          // Posa aquí la teva contrasenya de MySQL
     database: 'minierp',   // Canvia-ho pel nom exacte de la teva base de dades
     waitForConnections: true,
     connectionLimit: 10,
